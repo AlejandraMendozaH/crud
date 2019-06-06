166b30676c68a529bd33b28ff071e334f35b6e7b
Alfonso José Barroso Barajas <alfonsojbarroso@hotmail.com>
2019-06-05 23:11:50 -0600

    navegacion

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactoListComponent } from './components/contacto-list/contacto-list.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contacto', component: ContactoListComponent },
+  { path: '**', component: ContactoListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { ContactoFormComponent } from './components/contacto-form/contacto-form.component';
+import { ContactoListComponent } from './components/contacto-list/contacto-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    ContactoFormComponent,
+    ContactoListComponent
   ],
   imports: [
     BrowserModule,
